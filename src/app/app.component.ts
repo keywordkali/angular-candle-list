de366f5a46a494c072fd98fec98690cc4c48dff0
Kali Correll <kalicorrell18@gmail.com>
2020-11-11 00:35:23 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-candle-list';
+  btnStyle: string = "btn btn-primary";
+  candles: any[] = [];
+  candleCollection: any[] = [
+{ name: "Yankee Candle",  scent: "Vanilla",      color: "White" },
+{ name: "Harlem Candles", scent: "SugarCookie",  color: "Pink" },
+{ name: "Pretty Honest",  scent: "Spring",       color: "Green" },
+{ name: "La Botica",      scent: "Pine",         color: "Dark Green" },
+{ name: "Mecca Candle",   scent: "Peach",        color: "Orange" },
+{ name: "Chesapeke Bay",  scent: "Christmas",    color: "Red" },
+{ name: "White Barn",     scent: "Midnight",     color: "Black" },
+{ name: "Lenox Luxury",   scent: "Rose",         color: "Pink" },
+{ name: "PF Candle Co",   scent: "Lavender",     color: "Purple" },
+  ];
+  title = 'candle-list';
+  
+  getCandles(): void{
+    this.candles = this.candleCollection;
+  }
+
+ngOnInit(): void{
+
 }
+
+}
+
